7889580062dd70d4e9480ec01ce1b79228e493ca
Yzhan498 <yzhan498@my.centennialcollege.ca>
2023-04-01 14:35:33 -0400

    Initial Commit

---
+++ FacialRecognitionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacialRecognitionApp
+//
+//  Created by ying zhang on 2023-04-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
